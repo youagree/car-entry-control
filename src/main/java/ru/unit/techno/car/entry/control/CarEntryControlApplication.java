ac72aee8ec351b622e471b12b6a514ca42765526
docent42 <ermakov42@mail.ru>
2021-01-14 21:38:22 +0700

    application folders & config files

---
+++ src/main/java/ru/unit/techno/car/entry/control/CarEntryControlApplication.java
@@ -0,0 +1,13 @@
+package ru.unit.techno.car.entry.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarEntryControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarEntryControlApplication.class, args);
+	}
+
+}
