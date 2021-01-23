99191edcd84390467341e68f3a4b436244e3103e
docent42 <ermakov42@mail.ru>
2021-01-23 22:04:48 +0700

    event controller added

---
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/controller/EventController.java
@@ -0,0 +1,21 @@
+package ru.unit_techno.car_entry_control.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+@AllArgsConstructor
+public class EventController {
+
+  @PostMapping("/event")
+  @ResponseStatus(HttpStatus.OK)
+  public String eventHandler(String rfidLabel) {
+    return "access granted";
+  }
+
+}
