29d6c7f44969b7528291ffeb344c70cf0bdcc540
Eugene_Sleptsov <infralighter@gmail.com>
2021-02-23 21:18:02 +0300

    add DTO NewRfidLabelMessage description

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/response/NewRfidLabelMessage.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/response/NewRfidLabelMessage.java
@@ -2,6 +2,10 @@ package ru.unit_techno.car_entry_control.dto.response;
 
 import lombok.Data;
 
+/**
+ * !DTO для оповещения о созданиии новой метки
+ */
+
 @Data
 public class NewRfidLabelMessage {
 
