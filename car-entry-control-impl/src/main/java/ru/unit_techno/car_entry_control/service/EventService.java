ec8a8a44ae0958a55b8ecb978cbcb4422fda84b2
Alexey Vasin <aevasin@vtb.ru>
2021-03-15 20:23:40 +0300

    add logging for event controller

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/service/EventService.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/service/EventService.java
@@ -2,6 +2,7 @@ package ru.unit_techno.car_entry_control.service;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.unit_techno.car_entry_control.aspect.RfidEvent;
 import ru.unit_techno.car_entry_control.dto.request.RfidEntry;
@@ -14,6 +15,7 @@ import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventService {
@@ -22,29 +24,36 @@ public class EventService {
     private final RfidLabelRepository rfidLabelRepository;
 
     public Long rfidLabelCheck (RfidEntry rfidLabel) throws Exception {
+        log.info("start checking rfid label for access entry device: {}", rfidLabel);
         Long longRfidLabel = rfidLabel.getRfid();
+        log.info("rfid id is: {}", longRfidLabel);
         Optional<RfidLabel> label = rfidLabelRepository.findByRfidLabelValue(longRfidLabel);
         rfidExceptionCheck(label);
+        log.info("finish validate rfid, start open entry device");
         return label.get().getId();
     }
 
     @RfidEvent
     public void create(Long rfidLabel) {
+        log.info("create new rfid label {}", rfidLabel);
         rfidLabelRepository.findByRfidLabelValue(rfidLabel).ifPresentOrElse(
                 r -> rfidLabelRepository.save(new RfidLabel().setRfidLabelValue(rfidLabel)
                         .setState(StateEnum.NEW)),
                 () -> {
+                    log.info("this rfid already exist");
                    throw new EntityExistsException();
                 } );
     }
 
     private void rfidExceptionCheck(Optional<RfidLabel> rfidLabel) throws RfidAccessDeniedException {
         if (rfidLabel.isEmpty()) {
+            log.info("rfidLabel is empty, not exist");
             throw new EntityNotFoundException("this rfid label is not in the database");
         }
 
         if (rfidLabel.get().getState().equals(StateEnum.NO_ACTIVE) ||
             rfidLabel.get().getState().equals(StateEnum.NEW)) {
+            log.info("rfidLabel is not active");
             notificationService.sendNotActive(rfidLabel.get().getRfidLabelValue());
             throw new RfidAccessDeniedException("this rfid label is not active");
         }
