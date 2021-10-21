8e69132f4a4be313b0c996c80808349ecaf7616e
Васин Алексей Евгеньевич <empty>
2021-10-21 21:19:46 +0300

    remove post contruct

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/CarEntryControlApplication.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/CarEntryControlApplication.java
@@ -1,6 +1,5 @@
 package ru.unit_techno.car_entry_control;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,13 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.unit_techno.car_entry_control.entity.RfidLabel;
-import ru.unit_techno.car_entry_control.repository.RfidLabelRepository;
-
-import javax.annotation.PostConstruct;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.util.Calendar;
 
 @EnableScheduling
 @EnableFeignClients(basePackages =
@@ -38,21 +30,7 @@ import java.util.Calendar;
 		"ru.unit.techno.ariss.log.action.lib.repository"})
 public class CarEntryControlApplication {
 
-	@Autowired
-	RfidLabelRepository repository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarEntryControlApplication.class, args);
-	}
-
-
-	@PostConstruct
-	public void init() {
-		repository.save(
-				new RfidLabel()
-						.setRfidLabelValue(123L)
-						.setCreationDate(new Timestamp(Calendar.getInstance().getTimeInMillis()))
-						.setBeforeActiveUntil(LocalDate.now())
-						.setNoActiveUntil(LocalDate.now().plusMonths(1)));
 	}
 }
