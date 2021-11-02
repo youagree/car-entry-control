4438c770dfb0e44a516758aedb66b7df3dec01a6
Васин Алексей Евгеньевич <empty>
2021-11-02 15:08:13 +0300

    add pause interval

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/CardsWithRfidLabelsDto.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/CardsWithRfidLabelsDto.java
@@ -1,7 +1,10 @@
 package ru.unit_techno.car_entry_control.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import ru.unit_techno.car_entry_control.entity.enums.StateEnum;
+
+import java.time.LocalDate;
 
 @Data
 public class CardsWithRfidLabelsDto {
@@ -11,4 +14,8 @@ public class CardsWithRfidLabelsDto {
     private String carColor;
     private String governmentNumber;
     private StateEnum state;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate noActiveUntil;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate beforeActiveUntil;
 }
