817c8c1ce758a2c6abe1f50f7e905dd9a7db859c
Eugene_Sleptsov <infralighter@gmail.com>
2021-11-11 21:42:37 +0300

    revert CORS changes, add development frontend Origin

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/config/WebSocketConfig.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/config/WebSocketConfig.java
@@ -18,6 +18,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins(
+                        "http://localhost:3000",
+                        "https://ariss.lifo.ml"
+                )
                 .withSockJS();
     }
 }
