317c5a41649d32de5db7cbc0d614bdbf56865bfe
Васин Алексей Евгеньевич <empty>
2021-11-11 23:05:55 +0300

    add input param for state

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/controller/RfidController.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/controller/RfidController.java
@@ -11,7 +11,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.unit_techno.car_entry_control.dto.CarCreateDto;
 import ru.unit_techno.car_entry_control.dto.CardsWithRfidLabelsDto;
 import ru.unit_techno.car_entry_control.dto.RfidLabelDto;
@@ -80,7 +89,8 @@ public class RfidController {
     public Page<CardsWithRfidLabelsDto> findAllRfidsWithCard(
             @JoinFetch(paths = "car")
             @And({@Spec(path = "rfidLabelValue", params = "rfidLabelValue", spec = Equal.class),
-                    @Spec(path = "car.governmentNumber", params = "governmentNumber", spec = Equal.class)
+                    @Spec(path = "car.governmentNumber", params = "governmentNumber", spec = Equal.class),
+                    @Spec(path = "state", params = "state", spec = Equal.class)
             }) Specification<RfidLabel> specificationPageable, Pageable pageable) {
         return rfidService.getAllRfidsWithCars(pageable, specificationPageable);
     }
--- car-entry-control-impl/src/main/resources/test-data-cars.sql
+++ car-entry-control-impl/src/main/resources/test-data-cars.sql
@@ -1,22 +1,41 @@
 SET search_path TO car_entry_control;
 
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (92, false,  'О723ОН439', 'Жигуль', 'Белый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (946, true,  'У813АН24', 'Жигуль', 'Черный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (430, true,  'С573СМ651', 'BMW', 'Белый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (794, false, 'У813АН24', 'Жигуль', 'Красный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (418, true,  'Н046СВ080', 'Жигуль', 'Красный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (622, false, 'Т413УВ80', 'Audi', 'Зеленый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (261, true,  'Н046СВ080', 'Жигуль', 'Синий');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (144, true,  'В200ОМ77', 'BMW', 'Белый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (188, false, 'У813АН24', 'Жигуль', 'Синий');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (975, false, 'О225КУ73', 'Жигуль', 'Зеленый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (862, false, 'С600ХУ401', 'Жигуль', 'Синий');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (45, true,   'О723ОН439', 'BMW', 'Белый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (850, true,  'М217ТВ58', 'Лада', 'Красный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (891, false, 'О723ОН439', 'Audi', 'Зеленый');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (433, false, 'С573СМ651', 'Лада', 'Черный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (868, true,  'Т413УВ80', 'Жигуль', 'Синий');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (306, false, 'Т413УВ80', 'Audi', 'Черный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (104, true,  'У813АН24', 'Жигуль', 'Черный');
-insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (930, false, 'С573СМ651', 'Жигуль', 'Черный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (92, false, 'О723ОН439', 'Жигуль', 'Белый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (946, true, 'У813АН24', 'Жигуль', 'Черный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (430, true, 'С573СМ651', 'BMW', 'Белый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (794, false, 'У823АН24', 'Жигуль', 'Красный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (418, true, 'Н046СВ080', 'Жигуль', 'Красный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (622, false, 'Т413УВ80', 'Audi', 'Зеленый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (261, true, 'Н047СВ080', 'Жигуль', 'Синий');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (144, true, 'В200ОМ77', 'BMW', 'Белый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (188, false, 'У853АН24', 'Жигуль', 'Синий');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (975, false, 'О225КУ73', 'Жигуль', 'Зеленый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (862, false, 'С600ХУ401', 'Жигуль', 'Синий');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (45, true, 'О713ОН439', 'BMW', 'Белый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (850, true, 'М217ТВ58', 'Лада', 'Красный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (891, false, 'О743ОН439', 'Audi', 'Зеленый');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (433, false, 'С563СМ651', 'Лада', 'Черный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (868, true, 'Т414УВ80', 'Жигуль', 'Синий');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (306, false, 'Т453УВ80', 'Audi', 'Черный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (104, true, 'У873АН24', 'Жигуль', 'Черный');
+insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color)
+values (930, false, 'С523СМ651', 'Жигуль', 'Черный');
 insert into cars (car_id, car_on_the_territory, government_number, car_model, car_color) values (641, false, 'С190ХО30', 'Audi', 'Синий');
