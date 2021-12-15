b29082462c576b9a6904e333946405e3638b0a02
Васин Алексей Евгеньевич <empty>
2021-12-15 20:22:37 +0300

    fix json naming for correct sorting

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/CardsWithRfidLabelsDto.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/dto/CardsWithRfidLabelsDto.java
@@ -2,6 +2,7 @@ package ru.unit_techno.car_entry_control.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import ru.unit_techno.car_entry_control.entity.enums.StateEnum;
 
@@ -11,10 +12,12 @@ import java.time.LocalDate;
 public class CardsWithRfidLabelsDto {
 
     private Long rfidLabelValue;
+    @JsonProperty("car.carModel")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String carModel;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String carColor;
+    @JsonProperty("car.governmentNumber")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String governmentNumber;
     private StateEnum state;
