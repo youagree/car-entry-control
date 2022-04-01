d6155e7427cbc2aa1d658a70a106e1c533cd3c78
Alexey Vasin <alexey.vasin@convexitydmcc.com>
2022-04-01 17:29:51 +0400

    empty commit for rebuild lib

--- car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/util/Constant.java
+++ car-entry-control-impl/src/main/java/ru/unit_techno/car_entry_control/util/Constant.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Constant {
+
     public static final String REGEX = "(^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{4}(?<!0000)\\d{2,3})|(^\\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)[АВЕКМНОРСТУХ]\\d{2,3})$";
 
     public static final String RFID_NOT_FOUND_MESSAGE = "Попытка вьезда по неизвестной rfid метке";
